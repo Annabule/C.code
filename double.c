57f13acf58cb94b836bb76ce15a2d8fbe85e1fe5
Annabule <98166850+Annabule@users.noreply.github.com>
2022-02-08 21:16:15 +0800

    Add files via upload

---
+++ double.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+int main ()
+{  
+    printf("请分别输入身高的英尺和英寸，"
+       "如输入\"5 7 \"表示5英尺7英寸：");
+ double foot;
+double inch;
+scanf("%lf %lf",&foot,&inch) ; 
+printf("身高%f米。\n",
+      (foot+inch/12)*0.3048);
+	  return 0; 
+}
---
+++ 除法.c
@@ -0,0 +1,7 @@
+#include<stdio.h>   
+int main()
+{
+
+	printf("%f\n" ,14.0/3);
+	return 0 ; 
+}
